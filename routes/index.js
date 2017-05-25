0992d907a14f36f5675f162785fc0bd4e5347ed4
Unknown <hectorydoblado@gmail.com>
2017-05-24 18:44:37 -0600

    EJERCICIO IMAGEN  CONTACTO

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PROYECTO DE PRUEBA HECTOR DOBLADO' });
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto',{"img": "/images/imagen1.jpg" });
+  });
 });
 
 module.exports = router;
